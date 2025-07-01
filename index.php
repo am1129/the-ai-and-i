fae84d54296994fddc1dab596b2d2d040c05e7eb
am1129 <sato.sable1129@gmail.com>
2025-07-01 23:11:21 +0900

    最小構成ファイルを作成

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>The AI and I</title>
+  <link rel="stylesheet" href="assets/css/style.css?v=<?php echo time(); ?>">
+</head>
+<body>
+  <script src="assets/js/script.js?v=<?php echo time(); ?>"></script>
+</body>
+</html>
